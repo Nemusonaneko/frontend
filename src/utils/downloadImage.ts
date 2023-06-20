8ece21b6c4ae62a47b3f9f5e49f8c81819f76f96
nemusona <nemusonaneko@protonmail.com>
2023-06-20 02:44:12 -0500

    fix background

--- src/utils/downloadImage.ts
+++ src/utils/downloadImage.ts
@@ -2,7 +2,6 @@ import { GenDataValues, HistoryValues } from "@/types";
 
 export default function downloadImage(data: GenDataValues | HistoryValues) {
   const a = document.createElement("a");
-
   a.setAttribute("href", `data:image/png;base64,${data.base64}`);
   a.download = `nemuswaifugen-${data.base64.slice(100, 110)}-${data.seed}.png`;
   a.click();
