050a82328ac90e9fb53d5ff137f4070d9dfad50a
crossnetsneko <137281947+crossnetsneko@users.noreply.github.com>
2023-07-02 01:57:51 +0000

    mobile layout fixes

--- src/components/importTagsModal/MiniPreview.tsx
+++ src/components/importTagsModal/MiniPreview.tsx
@@ -20,10 +20,10 @@ export default function MiniPreview({src, info}){
 				<img
 					src={decodedImage}
 					alt="waifu"
-					className="grow aspect-square w-20 h-full md:w-full md:h-full rounded-t-lg"
+					className="grow aspect-square w-full h-full hidden md:block rounded-t-lg"
 				/>
 				<div /*Model, CFG Scale, Noise*/
-					className="absolute flex flex-row gap-2 modelInfo bg-[color:var(--bg2)] px-1.5 py-0.5 rounded-bl-lg rounded-tr-lg"
+					className="hidden md:flex absolute flex-row gap-2 modelInfo bg-[color:var(--bg2)] px-1.5 py-0.5 rounded-bl-lg rounded-tr-lg"
 					style={{top:"-1px", right:"-1px"}}
 				>
 					<div className="flex flex-col justify-center" style={{transform:"translateY(-1px)"}}>
@@ -36,7 +36,7 @@ export default function MiniPreview({src, info}){
 				</div>
 
 				<div /* Seed */
-					className="absolute flex flex-row gap-2 modelInfo bg-[color:var(--bg1)] px-1.5 py-0.5 rounded-tr-lg"
+					className="hidden md:flex absolute flex-row gap-2 modelInfo bg-[color:var(--bg1)] px-1.5 py-0.5 rounded-tr-lg"
 					style={{bottom:"-0px", left:"-0px"}}
 				>
 					<p>{info.metadata.get("Seed")}</p>
@@ -48,7 +48,7 @@ export default function MiniPreview({src, info}){
 				<p className="positive p-1 max-h-24 overflow-y-scroll bg-[color:var(--bg1)] text-[color:var(--green-70)]">
 					{info.positive}
 				</p>
-				<p className="negative p-1 max-h-16 overflow-y-scroll bg-[color:var(--bg1)] text-[color:var(--red-70)] rounded-b-lg">
+				<p className="negative p-1 max-h-16 overflow-y-scroll bg-[color:var(--bg1)] text-[color:var(--red-70)] md:rounded-b-lg">
 					{info.negative}
 			</p>
 			</div>
