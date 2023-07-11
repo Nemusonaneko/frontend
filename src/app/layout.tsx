f9358213e2fcc81b749df95e3ca630ae0bfb010f
nemusona <nemusonaneko@protonmail.com>
2023-07-10 21:29:25 -0400

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Nemu's Waifu Gen",
-  description: "Free Stable Diffusion interface for people who dont want to drop thousands on a good GPU UwU",
+  description:
+    "Stable Diffusion interface focused on waifus for people who dont want to drop thousands on a good GPU UwU",
   icons: { icon: "/favicon.ico" },
 };
-
 
 export default function RootLayout({
   children,
@@ -21,12 +21,26 @@ export default function RootLayout({
   return (
     <html lang="en" className="original">
       <head>
-        <link key="idx-url" rel="canonical" href="https://waifus.nemusona.com/" />
+        <link
+          key="idx-url"
+          rel="canonical"
+          href="https://waifus.nemusona.com/"
+        />
         <meta key="graph-title" property="og:title" content={metadata.title} />
-        <meta key="graph-desc" property="og:description" content={metadata.description} />
-        <meta key="graph-img" property="og:image" content="https://waifus.nemusona.com/cute.gif" />
+        <meta
+          key="graph-desc"
+          property="og:description"
+          content={metadata.description}
+        />
+        <meta
+          key="graph-img"
+          property="og:image"
+          content="https://waifus.nemusona.com/cute.gif"
+        />
       </head>
-      <body className={`${inter.className} bg-[color:var(--bg0)] text-[color:var(--text)]`}>
+      <body
+        className={`${inter.className} bg-[color:var(--bg0)] text-[color:var(--text)]`}
+      >
         <Providers>
           <NavBar />
           <div className="m-auto max-w-7xl px-1 py-1">{children}</div>
